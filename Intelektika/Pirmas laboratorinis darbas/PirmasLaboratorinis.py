0d8f13bd7a8da22bd788a6fe581a8c04e297e3b7
Eligijus <e.kiudys@gmail.com>
2020-02-16 22:02:48 +0200

    reading in rows try number 1

--- Intelektika/Pirmas laboratorinis darbas/PirmasLaboratorinis.py
+++ Intelektika/Pirmas laboratorinis darbas/PirmasLaboratorinis.py
@@ -21,12 +21,44 @@ class Data:
         self.utime = utime
 
 
+class Continuous:
+    def __init__(self, name, amount, missing, cardinality, min, max, quartile1, quartile3, average, median, standardDeviation):
+        self.name= name;
+        self.amount = amount
+        self.missing = missing
+        self.cardinality = cardinality
+        self.min = min
+        self.max = max
+        self.quartile1 = quartile1
+        self.quartile3 = quartile3
+        self.average = average
+        self.median = median
+        self.standardDeviation = standardDeviation
+
+
+class Categorical:
+    def __init__(self, name, amount, missing, cardinality, mod, modFrequency, modPercentage, mod2, modFrequency2, modPercentage2):
+        self.name = name
+        self.amount = amount
+        self.missing = missing
+        self.cardinality = cardinality
+        self.mod = mod
+        self.modFrequency = modFrequency
+        self.modPercentage = modPercentage
+        self.modFrequency2 = modFrequency2
+        self.modPercentage2 = modPercentage2
+
+
 index = 0
 dataList = []
-suma = 0;
+continuousData = []
 with open('transcoding_mesurment.tsv') as tsvfile:
   reader = csv.DictReader(tsvfile, dialect='excel-tab')
   for row in reader:
-    print(row)
+    # print(row)
+    for rowData in row:
+        print(rowData)
+
     tempData = Data(row['id'], row['duration'], row['codec'], row['bitrate'], row['framerate'], row['i'], row['p'], row['frames'], row['i_size'], row['p_size'], row['size'], row['o_codec'], row['o_bitrate'], row['o_framerate'], row['umem'], row['utime'])
     dataList.append(tempData)
+
