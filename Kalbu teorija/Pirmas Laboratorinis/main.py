72992e84aeee836e945237bfd4b2ed6be660f15a
Eligijus <>
2020-02-20 16:22:08 +0200

    PKT created functions

--- Kalbu teorija/Pirmas Laboratorinis/main.py
+++ Kalbu teorija/Pirmas Laboratorinis/main.py
@@ -1,4 +1,3 @@
-import math
 
 
 class TelephoneInfo:
@@ -21,83 +20,96 @@ class Data (TelephoneInfo, TelephoneCalls):
         self.name = name
         self.price = price
         self.time = time
-        # super().number = number
-        # super().code = code
-        # super().price = price
-        # super().name = name
-        # super().time = time
 
 
 
-info = False
-infoList = []
-callsList = []
-dataList = []
-f = open("test.txt", "r")
-split = []
-for i in f.readlines():
-    index = 0
-    if len(split) == 2 and info:
-        split = []
-    elif not info:
-        split = []
-
-    for string in i:
-        if string == " ":
-            split.append(i[0:index])
-            split.append(i[index + 1: len(i)])
-            break
-        index += 1
-    if index == len(i):
-        split.append(i.strip())
-
-    # print("{0} {1}, {2}".format(split[0].strip(), len(i), index))
-    # split = i.split(" ")
-    split[0] = split[0].strip()
-    if len(split) > 1 and not bool(info):
-        # print(split)
-        split[1] = split[1].strip()
-        data = split[1].split('$')
-        telephoneInfo = TelephoneInfo(split[0], data[0], float(data[1]) * 0.1)
-        infoList.append(telephoneInfo)
-    elif len(split) == 1 and split[0] == "000000" and not bool(info):
-        # print(split)
-        split = []
-        info = True
-    elif len(split) > 1 and bool(info):
-        split[1] = split[1].strip()
-        calls = TelephoneCalls(split[0], split[1])
-        callsList.append(calls)
-
-
-for calls in callsList:
-    state = False
-    for info in infoList:
-        if calls.number[0:len(info.code)] == info.code:
-            data = Data(calls.number, calls.number[len(info.code):len(calls.number)], info.name, info.price, calls.time)
-            dataList.append(data)
-            state = True
-            break
-        elif calls.number[0] != "0":
-            tempInfo = TelephoneInfo(calls.number, "Local", 0)
-            data = Data(calls.number, tempInfo.code, tempInfo.name, tempInfo.price, calls.time)
-            dataList.append(data)
-            state = True
-            break
-    if not bool(state):
-        data = Data(calls.number, -1, "Unknown", -1, calls.time)
-        dataList.append(data)
-
-
-fSave = open("data.txt", "w+")
-# fSave.write("{0:14} {1:16} {2:8} {3:6} {4:6} {5:6}\n".format("1", "17", "51", "56", "62", "69"))
-for data in dataList:
-    if data.code != -1:
-        fSave.write("{0:15} {1:16} {2:8} {3:6} {4:6} {5:.2f}\n".format(data.number, data.name, data.code, data.time, str(round(float(data.price) * 0.1, 2)), float(data.price) * 0.1 * float(data.time)))
-    else:
-        fSave.write("{0:15} {1:16} {2:8} {3:6} {4:6} {5:.2f}\n".format(data.number, data.name, "", data.time, "", float(data.price)))
-f.close()
 
 
 
+def dataRead():
+    data = []
+    file = open("test.txt", "r")
+    for dataFromFile in file:
+        data.append(dataFromFile)
+    file.close()
+    return data
+
+
+def splitLine(dataFromFile):
+    split = []
+    listForInfo = []
+    listForCalls = []
+    info = False
+    for i in dataFromFile:
+
+        index = 0
+        if len(split) == 2 and info:
+            split = []
+        elif not info:
+            split = []
+
+        for string in i:
+            if string == " ":
+                split.append(i[0:index])
+                split.append(i[index + 1: len(i)])
+                break
+            index += 1
+        if index == len(i):
+            split.append(i.strip())
+
+        # print("{0} {1}, {2}".format(split[0].strip(), len(i), index))
+        # split = i.split(" ")
+        split[0] = split[0].strip()
+        if len(split) > 1 and not bool(info):
+            # print(split)
+            split[1] = split[1].strip()
+            data = split[1].split('$')
+            telephoneInfo = TelephoneInfo(split[0], data[0], float(data[1]) * 0.1)
+            listForInfo.append(telephoneInfo)
+        elif len(split) == 1 and split[0] == "000000" and not bool(info):
+            # print(split)
+            split = []
+            info = True
+        elif len(split) > 1 and bool(info):
+            split[1] = split[1].strip()
+            calls = TelephoneCalls(split[0], split[1])
+            listForCalls.append(calls)
+    return listForInfo, listForCalls
+
+def calculatePrice(listOfCall, listOfInfo):
+    calculatedData = []
+    for calls in listOfCall:
+        state = False
+        for info in listOfInfo:
+            if calls.number[0:len(info.code)] == info.code:
+                data = Data(calls.number, calls.number[len(info.code):len(calls.number)], info.name, info.price, calls.time)
+                calculatedData.append(data)
+                state = True
+                break
+            elif calls.number[0] != "0":
+                tempInfo = TelephoneInfo(calls.number, "Local", 0)
+                data = Data(calls.number, tempInfo.code, tempInfo.name, tempInfo.price, calls.time)
+                calculatedData.append(data)
+                state = True
+                break
+        if not bool(state):
+            data = Data(calls.number, -1, "Unknown", -1, calls.time)
+            calculatedData.append(data)
+    return calculatedData
+
+def saveData(calculatedData):
+    fSave = open("data.txt", "w+")
+    # fSave.write("{0:14} {1:16} {2:8} {3:6} {4:6} {5:6}\n".format("1", "17", "51", "56", "62", "69"))
+    for data in calculatedData:
+        if data.code != -1:
+            fSave.write("{0:15} {1:16} {2:8} {3:6} {4:6} {5:.2f}\n".format(data.number, data.name, data.code, data.time, str(round(float(data.price) * 0.1, 2)), float(data.price) * 0.1 * float(data.time)))
+        else:
+            fSave.write("{0:15} {1:16} {2:8} {3:6} {4:6} {5:.2f}\n".format(data.number, data.name, "", data.time, "", float(data.price)))
+    fSave.close()
+
+
+fileData = dataRead()
+infoList, callsList = splitLine(fileData)
+dataList = calculatePrice(callsList, infoList)
+saveData(dataList)
 
