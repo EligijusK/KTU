ff9bd57b6814a92db3eea8a1a9712a3a27771461
Eligijus <e.kiudys@gmail.com>
2020-03-04 14:51:48 +0200

    pkt antro pradzia ir intelektikos ataskaita baigta

--- Kalbu teorija/Antras Laboratorinis/Scalatron/users/elikiu/versions/0/Bot.scala
+++ Kalbu teorija/Antras Laboratorinis/Scalatron/users/elikiu/versions/0/Bot.scala
@@ -1,15 +1,9 @@
-// Tutorial Bot #2: Counting Cycles
-
-class ControlFunction {
-    var n = 0
-    def respond(input: String) = {
-        val output = "Status(text=" + n + ")"   // temp value
-        n += 1                                  // now increments after use
-        output                                  // yield
-    }
-}
+// this is the source code for your bot - have fun!
 
 class ControlFunctionFactory {
-    def create = new ControlFunction().respond _
+    def create = new Bot().respond _
 }
 
+class Bot {
+    def respond(input: String) = "Status(text=elikiu)"
+}
